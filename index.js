577e7b3b34994a594ad9f565bebd4a576183f677
Harry Whyte~ <harrywhyte@kubrickgroup.com>
2023-09-12 16:41:14 +0100

    Slight format change

--- index.js
+++ index.js
@@ -30,8 +30,6 @@ app.get('/api/whoami', function(req, res) {
     {ipaddress: req.ip, language: req.headers['accept-language'], software: req.headers['user-agent']})
 })
 
-
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
